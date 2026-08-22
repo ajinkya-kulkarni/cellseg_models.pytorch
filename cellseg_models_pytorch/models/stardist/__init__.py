8bc2aa63c4adb0b5eb61b5d323104efe3c869ff6
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2026-08-22 16:33:17 +0200

    refactor: order StarDist exports safely

--- cellseg_models_pytorch/models/stardist/__init__.py
+++ cellseg_models_pytorch/models/stardist/__init__.py
@@ -1,4 +1,4 @@
-from .onnx import StarDistONNXWrapper, export_stardist_onnx
 from .stardist import StarDist
+from .onnx import StarDistONNXWrapper, export_stardist_onnx
 
 __all__ = ["StarDist", "StarDistONNXWrapper", "export_stardist_onnx"]
