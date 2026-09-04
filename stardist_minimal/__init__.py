a3153e50a634ce8afab83dfcaa6f61b9898ea934
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2026-09-04 09:30:47 +0200

    feat: expose StarDist training and evaluation API

--- stardist_minimal/__init__.py
+++ stardist_minimal/__init__.py
@@ -1,5 +1,14 @@
+from .losses import StarDistLoss
+from .metrics import panoptic_quality
 from .model import StarDist
 from .postprocess import postprocess_stardist
-from .targets import gen_stardist_maps
+from .targets import gen_dist_map, gen_stardist_maps
 
-__all__ = ["StarDist", "gen_stardist_maps", "postprocess_stardist"]
+__all__ = [
+    "StarDist",
+    "StarDistLoss",
+    "gen_dist_map",
+    "gen_stardist_maps",
+    "panoptic_quality",
+    "postprocess_stardist",
+]
